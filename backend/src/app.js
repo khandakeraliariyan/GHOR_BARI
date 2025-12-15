28d11c14f98b556bf980c49b15647ddbff1f14be
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-12-16 04:51:01 +0600

    run the initial server for testing

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("GhorBari Backend Running 🚀");
+});
+
+module.exports = app;
