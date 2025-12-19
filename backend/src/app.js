d9800b05e5a06912876f28d385f460043e6421af
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-12-19 14:19:42 +0600

    created property module

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const testRoutes = require("./routes/testRoutes");
 const nidRoutes = require("./routes/nidRoutes");
+const propertyRoutes = require("./routes/propertyRoutes");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/nid", nidRoutes);
+app.use("/api/properties", propertyRoutes);
 
 app.get("/", (req, res) => {
   res.send("GhorBari Backend Running 🚀");
