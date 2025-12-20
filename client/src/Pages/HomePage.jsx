2371611fe0e2aaee4853a824ebbc071c5df8780b
Samiunauntor <samiunalimauntor@gmail.com>
2025-12-20 21:38:56 +0600

    client : added get started section in homepage

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from "../Components/Banner.jsx";
 import WhyChooseUs from '../Components/WhyChooseUs.jsx';
 import HomePageStats from '../Components/HomePageStats.jsx';
+import BeginYourPropertyJourney from '../Components/BeginYourPropertyJourney.jsx';
 
 const HomePage = () => {
+   
+
     return (
         <div className='min-h-screen'>
             <Banner></Banner>
             <HomePageStats></HomePageStats>
             <WhyChooseUs></WhyChooseUs>
+            <BeginYourPropertyJourney></BeginYourPropertyJourney>
         </div>
     );
 };
