d4fd755d84913858a9022753d3a1b49975254f07
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-01-10 14:19:35 +0600

    added specific routes

--- backend/src/routes/nidRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import verifyToken from "../middleware/verifyToken.js";
-import { submitNID } from "../controllers/nidController.js";
-
-const router = express.Router();
-
-router.post("/submit", verifyToken, submitNID);
-
-export default router;
